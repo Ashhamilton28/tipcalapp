47f8d53c9e997f719de5c1a43458e8b33e6cd03d
Ashley <Ashhamilton22888@gmail.com>
2023-08-21 09:04:02 -0400

    finished mobile UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,55 @@
-import React from 'react'
+import React from "react";
+import TipAmountAndTotal from "./TipAmountAndTotal";
+import dollarSign from "/images/icon-dollar.svg"
+import person from "/images/icon-person.svg"
 
 function Form() {
+  const buttons = ['5%','20%','15%', '25%', '50%', 'Custom']
   return (
+
     <section>
-        {/* bill  */}
-        <div className='flex flex-col gap-1'>
-            <label className='text-xl text-Dark-grayish-cyan ' htmlFor="Bill">Bill</label>
-            <input type="text" />
-        </div>
+      {/* bill  */}
+      <div className="flex flex-col gap-1 relative">
+        <label className="text-xl text-Dark-grayish-cyan " htmlFor="Bill">
+          Bill
+        </label>
+        <img className="w-4 absolute top-10 left-2" src={dollarSign} alt="" />
+        <input
+        
+          className=" bg-Very-light-grayish-cyan shadow-lg py-2 px-4 text-right"
+          placeholder=""
+          type="text"
 
-        {/* tip buttons  */}
-        <div></div>
+        />
+      </div>
 
-        {/* number of people  */}
-        <div></div>
+      {/* select tip  */}
+      <div className="mt-6">
+        <label className="text-xl text-Dark-grayish-cyan" htmlFor="Tip">
+          Select Tip %
+        </label>
+        <div className="flex items-center flex-wrap gap-4 mt-4">
+
+          {/* something kissing from map  */}
+          {/* add active and hover states  */}
+          {/* add number of people input  */}
+          {buttons.map((label)=><button className="bg-Very-dark-cyan h-10 min-w-[150px] text-white rounded-md">
+            {label}
+          </button>)}
+         
+        </div> 
+      </div>
+
+      {/* number of people  */}
+      <div className="mt-6 relative">
+       <label className="text-xl text-Dark-grayish-cyan">Number of People</label>
+       <img className="w-4 absolute top-10 left-2" src={person} alt="" />
+       <input className="bg-Very-light-grayish-cyan shadow-lg py-2 px-4 w-full text-right" type="text" />
+      </div>
+
+      <TipAmountAndTotal />
     </section>
-  )
+  );
 }
 
-export default Form
+export default Form;
---
+++ src/components/TipAmountAndTotal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function TipAmountAndTotal() {
+  return (
+    <div className="mt-4 bg-Very-dark-cyan w-62 p-4 rounded-lg">
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h5 className="text-White ">Tip Amount</h5>
+          <p className="text-Light-grayish-cyan">/person</p>
+        </div>
+        <p className="text-Primary-Cyan text-3xl font-semibold">$4.27</p>
+      </div>
+
+      <div className="mt-8 flex justify-between">
+        <div>
+          <h5 className="text-White ">Total</h5>
+          <p className="text-Light-grayish-cyan">/person</p>
+        </div>
+        <p className="text-Primary-Cyan text-3xl font-semibold">$32.79</p>
+      </div>
+
+      <button className="bg-Primary-Cyan p-2 w-full mt-4 rounded-md text-Very-dark-cyan font-semibold text-2xl">RESET</button>
+    </div>
+  );
+}
+
+export default TipAmountAndTotal;
